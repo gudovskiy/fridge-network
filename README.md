d9fd8ce72262eff07a55ad61f2f94bf57ef875f4
gudovskiyd <denis.gudovskiyd@us.panasonic.com>
2019-12-19 19:00:24 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# fridge-network
+## Reasoning model for Fridge demo
+
+This is the code used to train models for *smart fridge* paper and demo:
+
+**[Smart Home Appliances: Chat with Your Fridge](https://arxiv.org/pdf/1912.?????.pdf)**
+
+This project forks [MAC](https://github.com/stanfordnlp/mac-network) project and modifies it for *smart fridge* application. You can use this code to reproduce our models.
+
+## Requirements
+- Tensorflow (originally has been developed with 1.3 but should work for later versions as well).
+- We have performed experiments on Nvidia P100 GPU.
+- See [`requirements.txt`](requirements.txt) for the required python3 packages and run `pip3 install -r requirements.txt` to install them.
+
+## Pre-processing
+Before training the model, we first have to generate the FRIDGR dataset and extract features for the images:
+
+### Dataset
+To generate dataset, use [FRIDGR dataset repository](https://github.com/gudovskiy/fridge-dataset/):
+```bash
+mv FRIDGR_SPLIT_scenes.json ./FRIDGR_v0.1/scenes/
+mv FRIDGR_SPLIT_questions.json ./FRIDGR_v0.1/data/
+mv images/* ./FRIDGR_v0.1/images/SPLIT/
+```
+
+The final command moves the generated dataset into the proper `data` directory, where we will put all the data files we use during training.
+
+### Feature extraction
+Extract ResNet-101 features for the FRIDGR train, val, and test images with the following commands:
+```bash
+CUDA_VISIBLE_DEVICES=0, python3 extract_features.py --input_image_dir ./images/train --output_h5_file ./data/train.h5 --batch_size 100
+CUDA_VISIBLE_DEVICES=1, python3 extract_features.py --input_image_dir ./images/val   --output_h5_file ./data/val.h5 --batch_size 100
+CUDA_VISIBLE_DEVICES=2, python3 extract_features.py --input_image_dir ./images/test  --output_h5_file ./data/test.h5 --batch_size 100
+```
+
+## Training 
+To train the model, run the following command:
+```bash
+python3 main.py --expName "fridgr_FeatCls_EmbRandom_CfgArgs0" --train --batchSize 64 --testedNum 10000 --epochs 25 --netLength 4 --gpus 0 @configs/args.txt
+python3 main.py --expName "fridgr_FeatCls_EmbRandom_CfgArgs2" --train --batchSize 64 --testedNum 10000 --epochs 40 --netLength 6 --gpus 1 @configs/args2.txt
+python3 main.py --expName "fridgr_FeatCls_EmbRandom_CfgArgs3" --train --batchSize 64 --testedNum 10000 --epochs 40 --netLength 6 --gpus 2 @configs/args3.txt
+python3 main.py --expName "fridgr_FeatCls_EmbRandom_CfgArgs4" --train --batchSize 64 --testedNum 10000 --epochs 40 --netLength 6 --gpus 3 @configs/args4.txt
+```
+
+First, the program preprocesses the FRIDGR questions. It tokenizes them and maps them to integers to prepare them for the network. It then stores a JSON with that information about them as well as word-to-integer dictionaries in the `./FRIDGR_v0.1/data` directory.
+
+Then, the program trains the model. Weights are saved by default to `./weights/{expName}` and statistics about the training are collected in `./results/{expName}`, where `expName` is the name we choose to give to the current experiment. 
+
+### Notes
+- The number of examples used for training and evaluation can be set by `--trainedNum` and `--testedNum` respectively.
+- You can use the `-r` flag to restore and continue training a previously pre-trained model. 
+- We recommend you to try out varying the number of MAC cells used in the network through the `--netLength` option to explore different lengths of reasoning processes.
+- Good lengths for FRIDGR are in the range of 4-16 (using more cells tends to converge faster and achieves a bit higher accuracy, while lower number of cells usually results in more easily interpretable attention maps). 
+
+### Model variants
+We have explored several variants of our model. We provide a few examples in `configs/args2-4.txt`. For instance, you can run the first by: 
+```bash
+python3 main.py --expName "experiment1" --train --batchSize 64 --testedNum 10000 --epochs 40 --netLength 6 @configs/args2.txt
+```
+- [`args2`](config/args2.txt) uses a non-recurrent variant of the control unit that converges faster.
+- [`args3`](config/args3.txt) incorporates self-attention into the write unit.
+- [`args4`](config/args4.txt) adds control-based gating over the memory.
+
+See [`config.py`](config.py) for further available options (Note that some of them are still in an experimental stage).
+
+## Evaluation
+To evaluate the trained model, and get predictions and attention maps, run the following: 
+```bash
+python3 main.py --expName "fridgr_FeatCls_EmbRandom_CfgArgs0" --finalTest --batchSize 64 --testedNum 10000 --netLength 4 --gpus 3 --getPreds --getAtt -r @configs/args.txt
+python3 main.py --expName "fridgr_FeatCls_EmbRandom_CfgArgs2" --finalTest --batchSize 64 --testedNum 10000 --netLength 6 --gpus 4 --getPreds --getAtt -r @configs/args2.txt
+python3 main.py --expName "fridgr_FeatCls_EmbRandom_CfgArgs3" --finalTest --batchSize 64 --testedNum 10000 --netLength 6 --gpus 5 --getPreds --getAtt -r @configs/args3.txt
+python3 main.py --expName "fridgr_FeatCls_EmbRandom_CfgArgs4" --finalTest --batchSize 64 --testedNum 10000 --netLength 6 --gpus 6 --getPreds --getAtt -r @configs/args4.txt
+```
+The command will restore the model we have trained, and evaluate it on the validation set. JSON files with predictions and the attention distributions resulted by running the model are saved by default to `./preds/{expName}`.
+
+- In case you are interested in getting attention maps (`--getAtt`), and to avoid having large prediction files, we advise you to limit the number of examples evaluated to 5,000-20,000.
+
+## Visualization
+After we evaluate the model with the command above, we can visualize the attention maps generated by running:
+```bash
+python3 visualization.py --expName "fridgr_FeatCls_EmbRandom_CfgArgs4" --tier val
+
+python3 visualization.py --expName "fridgr_FeatCls_EmbRandom_CfgArgs4" --tier test --imageBasedir ./images --dataBasedir ./FRIDGR_v0.1
+
+```
+(Tier can be set to `train` or `test` as well). The script supports filtering of the visualized questions by various ways. See [`visualization.py`](visualization.py) for further details.
---
+++ visualization.py
@@ -0,0 +1,241 @@
+import json
+import pandas
+import argparse 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+from scipy.misc import imread, imresize
+from matplotlib.colors import Normalize, LinearSegmentedColormap
+
+flatten = lambda ll: [e for l in ll for e in l]
+
+parser = argparse.ArgumentParser()
+
+# experiment settings
+parser.add_argument("--tier",  default = "val", choices = ["train", "val", "test"], type = str)
+parser.add_argument("--expName",  default = "fridgrExperiment0", type = str)
+
+# plotting
+parser.add_argument("--cmap", default = "custom", type = str) # "gnuplot2", "GreysT" 
+
+parser.add_argument("--trans", help = "transpose question attention", action = "store_true")
+parser.add_argument("--sa", action = "store_true")
+parser.add_argument("--gate", action = "store_true")
+
+# filtering
+parser.add_argument("--instances", nargs = "*", type = int)
+parser.add_argument("--maxNum", default = 0, type = int)
+
+parser.add_argument("--filter", default = [], nargs = "*", choices = ["mod", "length", "field"])
+parser.add_argument("--filterMod", action = "store_true")
+parser.add_argument("--filterLength", type = int) # 19
+parser.add_argument("--filterField", type = str)
+parser.add_argument("--filterIn", action = "store_true")
+parser.add_argument("--filterList", nargs = "*") # ["how many", "more"], numbers
+
+# Dirs
+parser.add_argument("--imageBasedir", default = "./FRIDGR_v0.1", type = str, help = "image base directory")
+parser.add_argument("--dataBasedir", default = "./", type = str, help = "data base directory")
+parser.add_argument("--dataset", default = "FRIDGR", choices = ["FRIDGR", "CLEVR", "NLVR"], type = str)
+
+args = parser.parse_args()
+
+isRight = lambda instance: instance["answer"] == instance["prediction"]
+isRightStr = lambda instance: "RIGHT" if isRight(instance) else "WRONG"
+
+# files
+# jsonFilename = "valHPredictions.json" if args.humans else "valPredictions.json"
+imagesDir = "{dir}/{tier}".format(
+    dir = args.imageBasedir,
+    tier = args.tier)
+
+dataFile = "{dir}/preds/{expName}/{tier}Predictions-{expName}.json".format(
+    dir = args.dataBasedir,
+    tier = args.tier, 
+    expName = args.expName)
+
+inImgName = lambda index: "{dir}/{dataset}_{tier}_{index}.png".format(
+    dir = imagesDir,
+    dataset = args.dataset,
+    index = ("000000%d" % index)[-6:],
+    tier = args.tier)
+
+outImgAttName = lambda instance, j: "./preds/{expName}/{tier}/{id}Img_{step}.png".format(
+    expName = args.expName, 
+    tier = args.tier, 
+    id = instance["index"], 
+    step = j + 1)
+
+outTableAttName = lambda instance, name: "./preds/{expName}/{tier}/{id}{tableName}_{right}{orientation}.png".format(
+    expName = args.expName, 
+    tier = args.tier, 
+    id = instance["index"], 
+    tableName = name, 
+    right = isRightStr(instance), 
+    orientation = "_t" if args.trans else "")
+
+# plotting
+imageDims = (14,14)
+figureImageDims = (2,3)
+figureTableDims = (5,4)
+fontScale = 1
+
+# set transparent mask for low attention areas  
+# cdict = plt.get_cmap("gnuplot2")._segmentdata
+cdict = {"red": ((0.0, 0.0, 0.0), (0.6, 0.8, 0.8), (1.0, 1, 1)), 
+    "green": ((0.0, 0.0, 0.0), (0.6, 0.8, 0.8), (1.0, 1, 1)), 
+    "blue": ((0.0, 0.0, 0.0), (0.6, 0.8, 0.8), (1.0, 1, 1))}
+cdict["alpha"] = ((0.0, 0.35, 0.35),
+                  (1.0,0.65, 0.65))
+plt.register_cmap(name = "custom", data = cdict)
+
+def savePlot(fig, fileName):
+    plt.savefig(fileName, dpi = 720)
+    plt.close(fig) 
+    del fig
+
+def filter(instance):
+    if "length" in args.filter: 
+        if len(instance["question"].split(" ")) > args.filterLength:
+             return True
+
+    if "field" in args.filter:
+        if args.filterIn:  
+            if not (instance[args.filterField] in args.filterList):
+                return True
+        else:
+            if not any((l in instance[args.filterField]) for l in args.filterList):
+                return True            
+
+    if "mod" in args.filter:
+        if (not isRight(instance)) and args.filterMod:
+            return True
+
+        if isRight(instance) and (not args.filterMod):
+            return True
+
+    return False
+
+def showImgAtt(img, instance, step, ax):
+    dx, dy = 0.05, 0.05
+    x = np.arange(-1.5, 1.5, dx)
+    y = np.arange(-1.0, 1.0, dy)
+    X, Y = np.meshgrid(x, y)
+    extent = np.min(x), np.max(x), np.min(y), np.max(y)
+
+    ax.cla()
+
+    img1 = ax.imshow(img, interpolation = "nearest", extent = extent)
+    ax.imshow(np.array(instance["attentions"]["kb"][step]).reshape(imageDims), cmap = plt.get_cmap(args.cmap), 
+        interpolation = "bicubic", extent = extent)
+
+    ax.set_axis_off()
+    plt.axis("off")
+
+    ax.set_aspect("auto")
+
+
+def showImgAtts(instance):
+    img = imread(inImgName(instance["imageId"]))
+
+    length = len(instance["attentions"]["kb"])
+    
+    # show images
+    for j in range(length):
+        fig, ax = plt.subplots()
+        fig.set_figheight(figureImageDims[0])
+        fig.set_figwidth(figureImageDims[1])              
+        
+        showImgAtt(img, instance, j, ax)
+        
+        plt.subplots_adjust(bottom = 0, top = 1, left = 0, right = 1)
+        savePlot(fig, outImgAttName(instance, j))
+
+def showTableAtt(instance, table, x, y, name):
+    # if args.trans:
+    #     figureTableDims = (len(y) / 2 + 4, len(x) + 2)
+    # else:
+    #     figureTableDims = (len(y) / 2, len(x) / 2)
+    # xx = np.arange(0, len(x), 1)
+    # yy = np.arange(0, len(y), 1)
+    # extent2 = np.min(xx), np.max(xx), np.min(yy), np.max(yy)
+    
+    fig2, bx = plt.subplots(1, 1) # figsize = figureTableDims
+    bx.cla()
+
+    sns.set(font_scale = fontScale)
+
+    if args.trans:
+        table = np.transpose(table)
+        x, y = y, x
+    #print(table.shape, len(x), len(y))
+    tableMap = pandas.DataFrame(data = table, index = x, columns = y)
+    
+    bx = sns.heatmap(tableMap, cmap = "Purples", cbar = False, linewidths = .5, linecolor = "gray", square = True)
+    
+    # x ticks
+    if args.trans:
+        bx.xaxis.tick_top()
+    locs, labels = plt.xticks()
+    if args.trans:
+        plt.setp(labels, rotation = 0)
+    else:
+        plt.setp(labels, rotation = 60)
+
+    # y ticks
+    locs, labels = plt.yticks()
+    plt.setp(labels, rotation = 0)
+
+    plt.savefig(outTableAttName(instance, name), dpi = 720)
+
+def main():
+    with open(dataFile) as inFile:
+        results = json.load(inFile)
+
+    # print(args.exp)
+
+    count = 0
+    if args.instances is None:
+        args.instances = range(len(results))
+
+    for i in args.instances:        
+        if filter(results[i]):
+            continue
+
+        if count > args.maxNum and args.maxNum > 0:
+            break
+        count += 1
+
+        length = len(results[i]["attentions"]["kb"])
+        showImgAtts(results[i])
+
+        iterations = range(1, length + 1)
+        questionList = results[i]["question"].split(" ")
+        table = np.array(results[i]["attentions"]["question"])[:,:(len(questionList) + 0)]# + 1)]
+        #print(iterations, questionList)
+        showTableAtt(results[i], table, iterations, questionList, "text")
+        
+        if args.sa:
+            iterations = range(length)
+            sa = np.zeros((length, length))
+            for i in range(length):
+                for j in range(i+1):
+                    sa[i][j] = results[i]["attentions"]["self"][i][j]
+            
+            showTableAtt(results[i], sa[i][j], iterations, iterations, "sa")                    
+
+        print(i)
+        print("id:", results[i]["index"])        
+        print("img:", results[i]["imageId"])
+        print("Q:", results[i]["question"])
+        print("G:", results[i]["answer"])
+        print("P:", results[i]["prediction"])
+        print(isRightStr(results[i]))
+
+        if args.gate:
+            print(results[i]["attentions"]["gate"])
+
+        print("________________________________________________________________________")
+
+if __name__ == "__main__":
+    main()
